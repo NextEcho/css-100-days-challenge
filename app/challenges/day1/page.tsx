1dbc5b80063d7c39538679c0267b1e09c94ac853
NextEcho <nextecho@outlook.com>
2024-11-01 23:54:42 +0800

    feat(day01): finish day01 css challenge.

--- app/challenges/day1/page.tsx
+++ app/challenges/day1/page.tsx
@@ -4,6 +4,12 @@ export default function Day01() {
 	return (
 		<div className={`${styles.page}`}>
 			<main className={`${styles.container}`}>
+				<div className={`${styles.number}`}>
+					<div className={`${styles.firstOne}`}></div>
+					<div className={`${styles.secondOne}`}></div>
+					<div className={`${styles.firstZero}`}></div>
+					<div className={`${styles.secondZero}`}></div>
+				</div>
 				<div className={`${styles.fontLarge}`}>DAYS</div>
 				<div className={`${styles.fontSmall}`}>CSS CHALLENGE</div>
 			</main>
